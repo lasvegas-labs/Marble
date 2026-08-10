82e54acc7318eeb8887e41927012e7fd532b38b0
fahmi <fahmianton@gmail.com>
2026-08-10 10:51:59 +0700

    Initial Commit

---
+++ Marble/MarbleApp.swift
@@ -0,0 +1,17 @@
+//
+//  MarbleApp.swift
+//  Marble
+//
+//  Created by Muhammad Fahmi on 10/08/26.
+//
+
+import SwiftUI
+
+@main
+struct MarbleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
